b3e86659604c95dc4cdeff98a74366d88b179954
Damanii <mkmatthew999@gmail.com>
2016-05-25 17:14:28 -0400

    Store Outline Code

--- Store.java
+++ Store.java
@@ -9,10 +9,51 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 public class Store
 {
+  private int size;
+  private int stars;
+  private int profitability;
+  private int cost;
   public Store()
   {
   }
-  public Store(String Name, int size, int stars, int profiabililty, int cost)
+  public Store(String Name, int size, int stars, int profitability, int cost)
   {
+    this.size=size;
+    this.stars=stars;
+    this.profitability=profitability;
+    this.cost=cost;
+  }
+  public double calculateRevenue()
+  {
+    if(stars==1)
+    {
+    }
+    else if(stars==2)
+    {
+    }
+    else if(stars==3)
+    {
+    }
+    else if(stars==4)
+    {
+    }
+    else
+    {
+    }
+  }
+  public double calculateExpenses()
+  {
+    if(size==1)
+    {
+    }
+    else if(size==2)
+    {
+    }
+    else if(size==3)
+    {
+    }
+    else
+    {
+    }
   }
 }
